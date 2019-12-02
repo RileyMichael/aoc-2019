60bdf6af64f4ea379393b0defe14351e5e280f84
RileyMichael <riley.mcgee@centurylink.com>
2019-12-02 00:26:17 -0600

    day 2

---
+++ src/main/kotlin/com/github/rileymichael/aoc2019/Day02.kt
@@ -0,0 +1,47 @@
+package com.github.rileymichael.aoc2019
+
+fun main() {
+    val input = readInput("day02")
+        .flatMap { it.split(',') }
+        .map { it.toInt() }
+
+    // Part 1
+    val part1 = run(input.toMutableList(), 12, 2)
+    println("Part 1: ${part1[0]}")
+
+    // Part 2
+    val part2 = findSolution(input.toMutableList(), 19690720)
+    println("Part 2: ${100 * part2.first + part2.second}")
+}
+
+fun run(list: MutableList<Int>, noun: Int, verb: Int): MutableList<Int> {
+    var p = 0
+    list[1] = noun
+    list[2] = verb
+
+    while (list[p] != 99) {
+        when (list[p]) {
+            1 -> {
+                list[list[p + 3]] = list[list[p + 1]] + list[list[p + 2]]
+            }
+            2 -> {
+                list[list[p + 3]] = list[list[p + 1]] * list[list[p + 2]]
+            }
+        }
+        p += 4
+    }
+    return list
+}
+
+
+fun findSolution(list: List<Int>, desired: Int): Pair<Int, Int> {
+    for (noun in 0..99) {
+        for (verb in 0..99) {
+            if (run(list.toMutableList(), noun, verb)[0] == desired) {
+                return Pair(noun, verb)
+            }
+        }
+    }
+
+    throw NoSuchElementException("Solution not found for desired value.")
+}
---
+++ src/test/kotlin/com/github/rileymichael/aoc2019/Day02Test.kt
@@ -0,0 +1,34 @@
+package com.github.rileymichael.aoc2019
+
+import org.junit.jupiter.api.*
+import org.junit.jupiter.params.*
+import org.junit.jupiter.params.provider.*
+
+class Day02Test {
+    @ParameterizedTest
+    @MethodSource("part1")
+    fun part1(input: String, expected: String) {
+        val inputList = input.split(',').map { it.toInt() }
+        val expectedList = expected.split(',').map { it.toInt() }
+
+        val actual = run(inputList.toMutableList(), inputList[1], inputList[2])
+
+        Assertions.assertEquals(expectedList, actual)
+    }
+
+    @Test
+    fun part2() {
+        // no example..
+    }
+
+
+    companion object {
+        @JvmStatic
+        fun part1() = listOf(
+            Arguments.of("1,0,0,0,99", "2,0,0,0,99"),
+            Arguments.of("2,3,0,3,99", "2,3,0,6,99"),
+            Arguments.of("2,4,4,5,99,0", "2,4,4,5,99,9801"),
+            Arguments.of("1,1,1,4,99,5,6,0,99", "30,1,1,4,2,5,6,0,99")
+        )
+    }
+}
